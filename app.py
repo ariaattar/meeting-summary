d4836180856249f6049e73394ee5abe74adb33ab
ariaattar <72599441+ariaattar@users.noreply.github.com>
2023-06-29 00:00:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 from langchain.chat_models import ChatAnthropic
 import os
@@ -13,35 +12,34 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
+password = st.text_input("Enter the password:", type="password")
 
-# Use the secrets dict to access the secret value
-api_key = st.secrets["ANTHROPIC_API_KEY"]
-os.environ["ANTHROPIC_API_KEY"] = api_key
+if password == st.secrets["PASSWORD"]:
+    api_key = st.secrets["ANTHROPIC_API_KEY"]
+    os.environ["ANTHROPIC_API_KEY"] = api_key
 
+    from langchain.prompts.chat import (
+        ChatPromptTemplate,
+        SystemMessagePromptTemplate,
+        AIMessagePromptTemplate,
+        HumanMessagePromptTemplate,
+    )
+    from langchain.schema import (
+        AIMessage,
+        HumanMessage,
+        SystemMessage
+    )
+    chat = ChatAnthropic(model="claude-instant-1-100k", max_tokens_to_sample=10000)
 
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    AIMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage
-)
-chat = ChatAnthropic(model="claude-instant-1-100k", max_tokens_to_sample=10000)
+    transcript = st.text_input("Please enter your transcript: ")
 
-# Use Streamlit to get the transcript from the user
-transcript = st.text_input("Please enter your transcript: ")
+    if transcript:
+        nbm_template = "Summarize this call in current state, future state, required capabilities and negative consequences format, make it detailed and business focused. Highlight all details mentioned in the call."
+        messages = [
+            HumanMessage(content=f"""{nbm_template},  \n\n {transcript}""")
+        ]
+        answer = chat(messages)
 
-# If the user has entered a transcript, process it
-if transcript:
-    nbm_template = "Summarize this call in current state, future state, required capabilities and negative consequences format, make it detailed and business focused. Highlight all details mentioned in the call."
-    messages = [
-        HumanMessage(content=f"""{nbm_template},  \n\n {transcript}""")
-    ]
-    answer = chat(messages)
-
-    # Use Streamlit to display the summary
-    st.write(answer.content)
+        st.write(answer.content)
+else:
+    st.error("The password you entered is incorrect. Please try again.")
