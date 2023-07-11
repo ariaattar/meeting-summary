dc4409219368f3e921f361267b0f32c342d020aa
ariaattar <72599441+ariaattar@users.noreply.github.com>
2023-07-11 11:03:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,70 +1,45 @@
 import streamlit as st
-
 from langchain.chat_models import ChatAnthropic
-from langchain.prompts.chat import *
+from langchain.prompts.chat import * 
 from langchain.schema import *
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-
 import os
 
-st.markdown("<h1 style='color: green;'>🍃 Meeting Summarizer</h1>", unsafe_allow_html=True)
+st.set_page_config(page_title="Meeting Summarizer", page_icon="📝") 
+st.title("Meeting Summarizer 📝")
 
-st.markdown("""
-<style>
-.stTextInput input {
-border-color: green !important;   
-}
-</style>
-""", unsafe_allow_html=True)
 
-password = st.text_input("Enter the password:", type="password")
-
-if password == st.secrets["PASSWORD_KEY"]:
-
-  api_key = st.secrets["ANTHROPIC_API_KEY"]
-  os.environ["ANTHROPIC_API_KEY"] = api_key
-
-  chat = ChatAnthropic(
-    model="claude-2",
-    max_tokens_to_sample=10000,
-    streaming=True,  
-    verbose=True,
-    callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
-  )
-
-  # Generate summary
-  transcript = st.text_area("Please enter your transcript:")
-  if transcript:
-    nbm_template = """Summarize this call in  
-    Current State:
-    Future State: 
-    Required Capabilities:
-    Negative Consequences:
-    Roles/Responsibilities:
-    Format, make it detailed and business focused. Make sure you dont miss details mentioned in the call."""
-
-    messages = [
-      HumanMessage(content=f"""{nbm_template}, \\n\\n {transcript}""") 
-    ]
-
-    answer = chat(messages)
-    summary = answer.content
-    st.write(summary)
-
-    # Chatbot for followup
-    history = [summary] 
-    while True:
-      human_input = st.text_input("Ask a follow-up question:")
-      if human_input:
-        messages.append(HumanMessage(human_input))
-        answer = chat(messages, history)
-        bot_response = answer.content
-        st.write(bot_response)
-        history.append(human_input)
-        history.append(bot_response)
-      else:
-        break
-
-else:
+password = st.text_input("Enter password:", type="password")
+if password != st.secrets["password"]:
   st.error("Incorrect password")
+  st.stop()
+
+
+api_key = st.secrets["ANTHROPIC_API_KEY"]
+os.environ["ANTHROPIC_API_KEY"] = api_key
+
+chat = ChatAnthropic(
+  model="claude-instant-1.1-100k",
+  max_tokens_to_sample=1000,
+  verbose=True,
+  callback_manager=CallbackManager([StreamingStdOutCallbackHandler()])
+)
+
+transcript = st.text_area("Enter transcript:")
+if transcript:
+
+  summary = generate_summary(transcript, chat)
+  st.write(summary)
+
+
+history = [summary]
+while True:
+  question = st.text_input("Ask a follow-up question:")
+  if question:
+    history.append(str(question)) 
+    response = chat.ask(question, history)
+    st.write(response)
+    history.append(str(response))
+  else:
+    break
