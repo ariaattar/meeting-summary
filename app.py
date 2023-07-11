7104220194ca22b055b47c6dc0e61e5bba432800
ariaattar <72599441+ariaattar@users.noreply.github.com>
2023-07-11 11:07:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,67 @@
+
+
 import streamlit as st
 from langchain.chat_models import ChatAnthropic
-from langchain.prompts.chat import *
-from langchain.schema import * 
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
 import os
 
-st.set_page_config(page_title="Meeting Summarizer", page_icon="📝")
-st.title("Meeting Summarizer 📝")
+st.markdown("<h1 style='color: green;'>🍃 Meeting Summarizer</h1>", unsafe_allow_html=True)
 
-password = st.text_input("Enter password:", type="password") 
-if password != st.secrets["password"]:
-  st.error("Incorrect password")
-  st.stop()
+st.markdown("""
+<style>
+.stTextInput input {
+    border-color: green !important;
+}
+</style>
+""", unsafe_allow_html=True)
 
-api_key = st.secrets["ANTHROPIC_API_KEY"]
-os.environ["ANTHROPIC_API_KEY"] = api_key
+password = st.text_input("Enter the password:", type="password")
 
-chat = ChatAnthropic(
-  model="claude-instant",
-  max_tokens_to_sample=1000,
-  verbose=True,
-  callback_manager=CallbackManager([StreamingStdOutCallbackHandler()])
-)
+if password == st.secrets["PASSWORD_KEY"]:
+    api_key = st.secrets["ANTHROPIC_API_KEY"]
+    os.environ["ANTHROPIC_API_KEY"] = api_key
 
-transcript = st.text_area("Enter transcript:")
-if transcript:
-  summary = generate_summary(transcript, chat)
-  st.write(summary)
+    from langchain.prompts.chat import (
+        ChatPromptTemplate,
+        SystemMessagePromptTemplate,
+        AIMessagePromptTemplate,
+        HumanMessagePromptTemplate,
+    )
+    from langchain.schema import (
+        AIMessage,
+        HumanMessage,
+        SystemMessage
+    )
+    chat = ChatAnthropic(
+        model="claude-instant", 
+        max_tokens_to_sample=10000,
+        streaming=True,
+        verbose=True,
+        callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+    )
+    transcript = st.text_input("Please enter your transcript: ")
+    
+    if transcript:
+        nbm_template = """Summarize this call in
+        
+        Current State: 
+        
+        Future State: 
+        
+        Required Capabilities:
+        
+        Negative Consequences:
 
-  history = [summary]
-  while True:
-    question = st.text_input("Ask a follow-up question:")
-    if question:
-      history.append(str(question))
-      response = chat.ask(question, history)  
-      st.write(response)
-      history.append(str(response))
-    else:
-      break
+        Roles/Responsibilities:
+        
+        Format, make it detailed and business focused. Make sure you dont miss details mentioned in the call."""
+        messages = [
+            HumanMessage(content=f"""{nbm_template},  \n\n {transcript}""")
+        ]
+        answer = chat(messages)
+
+        st.write(answer.content)
+else:
+    st.error("The password you entered is incorrect. Please try again.")
